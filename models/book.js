da5bb586b8bced562e5cbb1a69f594e10a2ab619
Victoire <victoirebaron@hotmail.fr>
2019-07-28 14:21:29 -0700

    connect to MongoDB and create a Book schema

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+ title: { type: String, required: true },
+ authors: { type: String, required: true },
+ description: { type: String, required: true },
+ image: { type: String, required: true },
+ link: { type: String, required: true }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
