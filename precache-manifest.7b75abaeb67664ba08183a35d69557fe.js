2991977740e83003d8c160e8203f96c6477ebb9c
Victoire <victoirebaron@hotmail.fr>
2019-07-28 13:20:41 -0700

    Updates

---
+++ precache-manifest.7b75abaeb67664ba08183a35d69557fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c974b51ec5fc2beb663b1e3cf386f016",
+    "url": "/Google-Books-Search/index.html"
+  },
+  {
+    "revision": "14fbe9407d5a92686ec1",
+    "url": "/Google-Books-Search/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Google-Books-Search/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "14fbe9407d5a92686ec1",
+    "url": "/Google-Books-Search/static/js/main.e34a8493.chunk.js"
+  },
+  {
+    "revision": "236493d5c39f80583f64",
+    "url": "/Google-Books-Search/static/js/runtime~main.c9f1c7d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Google-Books-Search/static/media/logo.5d5d9eef.svg"
+  }
+]);
